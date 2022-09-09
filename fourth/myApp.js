318b5aaeb5f2f5f67366bc57669745cbb9537cf9
franco1316 <francoacm1011000@gmail.com>
2022-09-08 21:20:16 -0500

    fourth week

--- fourth/myApp.js
+++ fourth/myApp.js
@@ -1,11 +1,11 @@
-function sumEvenFibonacci(n, previous = 1, current = 1, arrayFibonacci = [1]) {
+function fibonacci(n, previous = 1, current = 1, arrayFibonacci = [1]) {
     if (current > n) return arrayFibonacci;
     var temporal = previous;
     previous = current;
     current += temporal;
     arrayFibonacci.push(previous)
-    return sumEvenFibonacci(n, previous, current, arrayFibonacci);
+    return fibonacci(n, previous, current, arrayFibonacci);
 }
 
-let show = sumEvenFibonacci(1000)
+let show = fibonacci(1000)
 console.log(show)
